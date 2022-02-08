c971ec6ee7e6951780c1f91b15933eee0c83a972
mlyin <39784530+mlyin@users.noreply.github.com>
2022-02-07 22:39:18 -0500

    finalized project
    only did task but 1 its aight

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import courses from '../data/courses'
 
-export default () => (
+const CourseCard = ({dept, number, title, description}) => {
+
+  const [isDisplayed, setDisplay] = useState(false);
+
+  return (
+    <>
+      <p onClick={(e)=>{setDisplay(!isDisplayed)}} key={`${dept}-${number}`}>
+            {dept}
+            {' '}
+            {number}
+            {' - '}
+            {title}
+        </p>
+        {isDisplayed ? <p>{description}</p> : ''}
+    </>
+  )
+}
+
+const Courses = () => {
+
+  return (
   <>
-    {courses.map(({ dept, number }) => (
-      <p key={`${dept}-${number}`}>
-        {dept}
-        {' '}
-        {number}
-      </p>
+    {courses.map(({ dept, number, title, description }) => (
+      <CourseCard dept={dept} number={number} title={title} description={description}/>
     ))}
   </>
-)
+)}
+
+export default Courses
